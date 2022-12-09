e9fea38e59caed799da59543313cd49070a43a79
Somraj Mukherjee <iamsomraj@gmail.com>
2022-12-09 20:21:08 +0530

    📦 NEW: INTIAL SETUP for VueSelect

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
-<script setup></script>
+<script setup>
+import VueSelect from "./components/VueSelect.vue";
+const options = [
+  { label: "First", value: 1 },
+  { label: "Second", value: 2 },
+  { label: "Third", value: 3 },
+  { label: "Fourth", value: 4 },
+  { label: "Fifth", value: 5 },
+];
+const value = ref(1);
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <VueSelect :options="options" v-model="value" />
 </template>
---
+++ src/components/VueSelect.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+
+/* PROPS */
+const props = defineProps({
+  options: {
+    type: Array,
+    required: true,
+  },
+  value: {
+    type: Number,
+    required: true,
+  },
+});
+/* PROPS */
+
+/* EMITS */
+const emits = defineEmits(["update:modelValue", "input", "change"]);
+/* EMITS */
+const isOpen = ref(false);
+</script>
+
+<template>
+  <div>{{ isOpen }}</div>
+</template>
