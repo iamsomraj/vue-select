f58a0e61b8bc444deed6d996b3fccb662da222e9
Somraj Mukherjee <iamsomraj@gmail.com>
2022-12-10 12:03:21 +0530

    📦 NEW: added heading

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,13 @@ const value = ref([]);
 
 <template>
   <div
-    class="flex min-h-screen items-center justify-center bg-gradient-to-b from-yellow-100 to-red-100"
+    class="flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-b from-yellow-100 to-red-100"
   >
+    <h1
+      class="bg-gradient-to-r from-red-300 to-yellow-300 bg-clip-text text-5xl font-extrabold text-transparent"
+    >
+      Vue Select
+    </h1>
     <VueSelect :options="options" v-model="value" :multiple="true" />
   </div>
 </template>
